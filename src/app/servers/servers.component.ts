26fc60bb7e96e353d49c0ca7812d420ce94e151b
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-01 10:49:40 +0200

    Commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-servers]',
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `
-  <hr>Template:
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
