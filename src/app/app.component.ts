84c62322e843242c13fda06e338ecd207bf874b7
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-10 16:57:21 +0200

    Assignment 3 - Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  date = new Date();
+  secretOut = false;
+  logs = [];
+
+  switchSecretStatus() {
+    if (this.secretOut == false) {
+      this.secretOut = true;
+    } else {
+      this.secretOut = false;
+    }
+
+    // Timestamp
+    var date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let timeLog = hr + ':' + min + ':'  + sec + '"';
+
+    // ID
+    let idNum = this.logs.length + 1;
+
+    // Insert Data into log
+    this.logs.push('#ID ' + idNum + ' ' + timeLog);
+
+  }
+
+  getColor() {
+    if ( this.logs.length > 5 ) {
+       return 'blue';
+    } else {
+      return 'white';
+    }
+  }
+
 }
