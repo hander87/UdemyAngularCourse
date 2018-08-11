42a14e5d604afca2bb6d5480b4a17ca506f01af8
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-11 15:21:29 +0200

    Section 5 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,9 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
-import { HeaderComponent } from './header/header.component';
-
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
-    RecipeBookComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
