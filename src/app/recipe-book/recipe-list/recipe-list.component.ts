f9984c7f0df3f213a99a8c2877c26ebb5860fa32
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-11 14:27:14 +0200

    Commit

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is desc for Test Resipe', 'https://www.inspiredtaste.net/wp-content/uploads/2013/01/Roasted-Pepper-Pinwheel-Pastry-Recipe-2-1200.jpg'),
+    new Recipe('Test Recipe 2', 'This is ANOTHER desc for Test Resipe', 'https://minimalistbaker.com/wp-content/uploads/2016/08/AMAZING-Chickpea-SHAKSHUKA-1-Pot-30-minutes-so-much-plantprotein-vegan-glutenfree-plantbased-shakshuka-recipe-easy-healthy.jpg')
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.recipes[0].name);
   }
 
 }
