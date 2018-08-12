77a9b404ea00674c56854c6735a344f4629bb7c1
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-12 13:02:27 +0200

    Mini assignment - Added page nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  @Output() topmenuClicked: EventEmitter<string> = new EventEmitter();
+
+  @Input() pageStatus: string;
+
   constructor() { }
+
+  onSelect(site) {
+    this.topmenuClicked.emit(site);
+  }
 
   ngOnInit() {
   }
