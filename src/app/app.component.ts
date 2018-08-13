7d29b978d7d390f775ed25b7bf192c7623323ea3
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-13 11:57:26 +0200

    Assignment 5 Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
-import { AccountService } from './shared/account.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
 
-  accounts: { name:string, status: string }[] = [];
-
-  constructor(private AccountService: AccountService) {}
-
-  ngOnInit() {
-    this.accounts = this.AccountService.accounts;
+  onSetToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
   }
 
+  onSetToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
 }
