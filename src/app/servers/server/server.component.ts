33f9016f09c9e37030aa46a4940a772fb4080838
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-14 12:50:03 +0200

    Assigment complete - Needed to convert id from STRING to NUMBER

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,10 +11,29 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+
+    // this.server = {
+    //   id: this.route.snapshot.params['id'],
+    //   name: this.route.snapshot.params['name'],
+    //   status: this.route.snapshot.params['status']
+    // }
+    const id = +this.route.snapshot.params['id'];
+
+    this.server = this.serversService.getServer(id);
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id'])
+        }
+      )
+
   }
 
 }
