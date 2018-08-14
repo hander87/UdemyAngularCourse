b795820377054525b8f51cc28ed1009a3ea18e64
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-14 11:27:13 +0200

    Query/fragment Params for dynamic URLS: ?allowEdit=1#loading

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,9 +13,12 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
-  onLoadServers() {
+  onLoadServer(id: number) {
     // COmplex code... then navigate away? Use:
-    this.router.navigate(['/servers']);
+    this.router.navigate(
+      ['/servers', id, 'edit'],
+      {queryParams: {allowEdit: '1'},
+      fragment: 'loading'});
   }
 
 }
