668cbec6843cf2d182c061357eb5b39527b30c09
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-14 16:16:57 +0200

    Creating our own simple observable.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 import 'rxjs/Rx';
 
 @Component({
@@ -14,12 +15,36 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
 
-    const myNumbers = Observable.interval(1000);
-    myNumbers.subscribe(
-      (number: number) => {
-        console.log(number);
-      },
+    // const myNumbers = Observable.interval(1000);
+    // myNumbers.subscribe(
+    //   (number: number) => {
+    //     console.log(number);
+    //   },
+    // );
+
+    // My Observable
+    const myObs = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('First package')
+      }, 2000);
+      setTimeout(() => {
+        observer.next('Second package')
+      }, 4000);
+      setTimeout(() => {
+        // observer.error('Something went wrong...')
+        observer.complete();
+      }, 6000);
+      setTimeout(() => {
+        observer.next('Third unused package (will not output)')
+      }, 8000);
+    });
+
+    myObs.subscribe(
+      (data: string) => { console.log('Calling Obs : ' + data); },
+      (error: string) => { console.log('Calling Error: ' + error); },
+      () => { console.log('Completed'); }
     );
+
   }
 
 }
