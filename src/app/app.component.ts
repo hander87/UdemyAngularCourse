d86bedb7a982770abe36f72ad4926a0e3ceb57ea
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-18 11:01:50 +0200

    Section INIT - forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentPage: string = 'recipies';
-
-  activateTopMenuPages(page: string) {
-    this.currentPage = page;
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,40 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shared/shopping-list.service';
-import { AppRoutingModule } from './app-routes.module';
-import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
-    RecipeBookComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    AppRoutingModule
+    HttpModule
   ],
-  providers: [ShoppingListService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
