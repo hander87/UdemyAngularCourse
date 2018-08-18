6e2a0a331c0c0b3349cab35c43cdeeb31b273be2
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-18 13:11:34 +0200

    Assignment Submission

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form') subForm: NgForm;
+  defaultSubscription = 'basic';
+  subs = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  formSent = false;
+
+  onSubmit() {
+    console.log(this.subForm);
+
+    this.subs.email = this.subForm.value.email;
+    this.subs.subscription = this.subForm.value.subscription;
+    this.subs.password = this.subForm.value.password;
+    this.formSent = true;
+
+    console.log(this.subs.email);
+    console.log(this.subs.subscription);
+    console.log(this.subs.password);
+  }
 
 }
