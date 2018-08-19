92349f8193ec7b1f821c61c1fa201b32766517f8
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-19 14:32:51 +0200

    Section 17 INIT - PIPES

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shared/shopping-list.service';
-import { AppRoutingModule } from './app-routes.module';
-import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
-import { RecipeBookListService } from './shared/recipe-book.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
-    RecipeBookComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
-    AppRoutingModule
+    HttpModule
   ],
-  providers: [ShoppingListService, RecipeBookListService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
