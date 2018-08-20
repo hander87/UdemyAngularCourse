98b8904e3f50fca3684c72b396fc747453c5f7fe
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-20 16:46:09 +0200

    Optimization with Feature modules and shared modules

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -2,22 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
-import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipebook', pathMatch: 'full'},
-  {path: 'recipebook', component: RecipeBookComponent, children: [
-    {path: '', component: RecipeStartComponent},
-    {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
-    {path: ':id', component: RecipeDetailComponent},
-    {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]},
-  ]},
   {path: 'shoppinglist', component: ShoppingListComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'signin', component: SigninComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shared/shopping-list.service';
 import { AppRoutingModule } from './app-routes.module';
-import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
 import { RecipeBookListService } from './shared/recipe-book.service';
 import { DatabaseService } from './shared/database.service';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
+import { RecipebookModule } from './recipe-book/recipe-book.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -29,22 +24,16 @@ import { AuthGuard } from './auth/auth-guard.service';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipeBookComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    RecipeListComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecipebookModule,
+    SharedModule
   ],
   providers: [
     ShoppingListService,
