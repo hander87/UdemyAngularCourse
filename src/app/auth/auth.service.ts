ddfb93f35a91b11bdbe79aa68cc19d795856dc32
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-20 11:40:44 +0200

    Firebase Rule settings for auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,13 @@
 import * as firebase from 'firebase';
 
+// FIREBASE SETTINGS: RULES
+// {
+//   "rules": {
+//     ".read": "auth != null",
+//     ".write": "auth != null"
+//   }
+// }
+
 export class AuthService {
 
   signupUser(email: string, password: string) {
