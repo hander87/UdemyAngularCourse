33e63a3318dded86e6f63f19924d6e805f5bbd0e
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-21 10:30:56 +0200

    Intercepting and logging trafic

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Intercepted!', req);
     return next.handle(req);
   }
 }
