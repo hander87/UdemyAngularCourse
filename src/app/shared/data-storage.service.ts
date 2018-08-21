4ddd53c7d502c0ce349e292c152659d44f712f29
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-21 11:07:02 +0200

    Adjustment

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -12,6 +12,7 @@ export class DataStorageService {
               private recipeService: RecipeService,
               private authService: AuthService) {
   }
+  fblink = 'https://udemyangularcourse-courseapp.firebaseio.com';
 
   storeRecipes() {
     // const headers = new HttpHeaders().set('Authorization', 'Bearer afdklasflaldf');
@@ -21,20 +22,20 @@ export class DataStorageService {
     //   params: new HttpParams().set('auth', token)
     //   // headers: headers
     // });
-    const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
+    const req = new HttpRequest('PUT', this.fblink, this.recipeService.getRecipes(), {reportProgress: true});
     return this.httpClient.request(req);
   }
 
   getRecipes() {
     // this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json?auth=' + token)
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
+    this.httpClient.get<Recipe[]>(this.fblink, {
       observe: 'body',
       responseType: 'json'
     })
       .map(
         (recipes) => {
           console.log(recipes);
-          for (let recipe of recipes) {
+          for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
             }
