6b721df20510b4aa893e65cffe6e29ffabad5ed3
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-21 14:44:37 +0200

    Managing ALL State in NgRx

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,40 +1,28 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-// import { Subscription } from 'rxjs/Subscription';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
+import * as fromShoppingList from './store/shopping-list.reducers';
+import * as ShoppingListActions from './store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit, OnDestroy {
+export class ShoppingListComponent implements OnInit {
   shoppingListState: Observable<{ingredients: Ingredient[]}>;
-  // private subscription: Subscription;
 
   constructor(
-    private slService: ShoppingListService,
-    private store: Store<{shoppingList: {ingredients: Ingredient[]}}>
+    private store: Store<fromShoppingList.AppState>
   ) { }
 
   ngOnInit() {
     this.shoppingListState = this.store.select('shoppingList');
-    // this.subscription = this.slService.ingredientsChanged
-    //   .subscribe(
-    //     (ingredients: Ingredient[]) => {
-    //       this.ingredients = ingredients;
-    //     }
-    //   );
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
-  }
-
-  ngOnDestroy() {
-    // this.subscription.unsubscribe();
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 }
--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -2,10 +2,42 @@ import { Action } from '@ngrx/store';
 import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGRIDIENT = 'ADD_INGRIDIENT';
+export const ADD_INGRIDIENTS = 'ADD_INGRIDIENTS';
+export const UPDATE_INGRIDIENT = 'UPDATE_INGRIDIENT';
+export const DELETE_INGRIDIENT = 'DELETE_INGRIDIENT';
+export const START_EDIT = 'START_EDIT';
+export const STOP_EDIT = 'STOP_EDIT';
 
 export class AddIngridient implements Action {
   readonly type = ADD_INGRIDIENT;
-  payload: Ingredient;
-}
 
-export type ShoppingListActions = AddIngridient;
+  constructor(public payload: Ingredient ) {}
+}
+export class AddIngridients implements Action {
+  readonly type = ADD_INGRIDIENTS;
+
+  constructor(public payload: Ingredient[] ) {}
+}
+export class UpdateIngredient implements Action {
+  readonly type = UPDATE_INGRIDIENT;
+
+  constructor(public payload: {ingredient: Ingredient} ) {}
+}
+export class DeleteIngredient implements Action {
+  readonly type = DELETE_INGRIDIENT;
+}
+export class StartEdit implements Action {
+  readonly type = START_EDIT;
+
+  constructor(public payload: number ) {}
+}
+export class StopEdit implements Action {
+  readonly type = STOP_EDIT;
+}
+export type ShoppingListActions =
+AddIngridient |
+AddIngridients |
+UpdateIngredient |
+DeleteIngredient |
+StartEdit |
+StopEdit;
