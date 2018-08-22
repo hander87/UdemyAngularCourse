5f6b6d514d7e7ba144024922755c596dee442b91
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-22 16:04:34 +0200

    App Module adjustment for Global Reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
-import { shoppingListRecucer } from './shopping-list/store/shopping-list.reducers';
+import { reducers } from './store/app.reducers';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { shoppingListRecucer } from './shopping-list/store/shopping-list.reducer
     ShoppingListModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot({shoppingList: shoppingListRecucer})
+    StoreModule.forRoot(reducers)
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/auth/store/auth-actions.ts
@@ -0,0 +1,30 @@
+import { Action } from '@ngrx/Store';
+
+export const SIGNUP = 'SIGNUP';
+export const SIGNIN = 'SIGNIN';
+export const LOGOUT = 'LOGOUT';
+export const SET_TOKEN = 'LOGOUT';
+
+export class Signup implements Action {
+  readonly type = SIGNUP;
+}
+
+export class Signin implements Action {
+  readonly type = SIGNIN;
+}
+
+export class Logout implements Action {
+  readonly type = LOGOUT;
+}
+export class SetToken implements Action {
+  readonly type = SET_TOKEN;
+  constructor(public payload: string) {
+
+  }
+}
+export type AuthActions =
+Signup |
+Signin |
+Logout |
+SetToken;
+
