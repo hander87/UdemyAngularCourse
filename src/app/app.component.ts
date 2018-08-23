4262cce4809e36d05f75e6adf30aca2d8895fbb8
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-23 16:01:53 +0200

    Switching between multible states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,20 +14,47 @@ import { trigger , state , style, transition, animate } from '@angular/animation
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(400)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
       transition('normal => highlighted', animate(200)),
-      transition('highlighted => normal', animate(800)),
-    ])
+      transition('highlighted => normal', animate(900)),
+      transition('shrunken <=> *', animate(500)),
+    ]),
   ]
+
 })
 export class AppComponent {
   dstate = 'normal';
+  wildstate = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAnimate() {
       this.dstate === 'normal' ? this.dstate = 'highlighted' : this.dstate = 'normal';
+      this.wildstate === 'normal' ? this.wildstate = 'highlighted' : this.wildstate = 'normal';
+    }
+
+    onShrink() {
+      this.wildstate = 'shrunken';
     }
 
     onAdd(item) {
       this.list.push(item);
+    }
+
+    onDelete() {
+      return null;
     }
 }
