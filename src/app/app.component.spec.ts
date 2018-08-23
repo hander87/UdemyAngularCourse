40e15e70665ac0ea9e97ac2c270af7bdbcd00fe9
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-23 18:29:25 +0200

    Section init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,16 +8,20 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
+  // These are commands. Checks if Title is "app" for ex.
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
