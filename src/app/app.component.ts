3de7e88eb55bd2de1f8c94b74e993b5721bb6c81
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-23 15:15:06 +0200

    setting animation with @dState

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { trigger , state , style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('dState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  dstate = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
