a3c7e307787e6fcaa87b32f9666089f577db774b
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-23 17:19:57 +0200

    Section and App init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,139 +1,21 @@
-import { Component } from '@angular/core';
-import { trigger , state , style, transition, animate, keyframes, group } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [
-    trigger('dState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0)'
-      })),
-      state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(100px)'
-      })),
-      transition('normal <=> highlighted', animate(400)),
-    ]),
-    trigger('wildState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0) scale(1)'
-      })),
-      state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(100px) scale(1)'
-      })),
-      state('shrunken', style({
-        'background-color': 'green',
-        transform: 'translateX(0) scale(0.5)',
-      })),
-      transition('normal => highlighted', animate(200)),
-      transition('highlighted => normal', animate(900)),
-      transition('shrunken <=> *', [
-        style({
-          borderRadius: '0',
-          'background-color': 'orange'
-        }),
-        animate(1000, style({
-          borderRadius: '50px'
-        })),
-        animate(500)
-      ]),
-    ]),
-    // List1 Animations
-    trigger('list1', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('void => *', [
-        style({
-          opacity: 0,
-          transform: 'translateX(-100px)'
-        }),
-        animate(200)
-      ]),
-      transition('* => void', [
-        animate(200, style({
-          transform: 'translateX(100px)',
-          opacity: 0
-        }))
-      ]),
-    ]),
-    // List2 Animations
-    trigger('list2', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('void => *', [
-        animate(1000, keyframes([
-          style({
-            transform: 'translateX(-100px)',
-            opacity: 0,
-            offset: 0
-          }),
-          style({
-            transform: 'translateX(-50px)',
-            opacity: 0.5,
-            offset: 0.3
-          }),
-          style({
-            transform: 'translateX(-20px)',
-            opacity: 0.3,
-            offset: 0.8
-          }),
-          style({
-            transform: 'translateX(0px)',
-            opacity: 1,
-            offset: 1
-          }),
-        ]))
-      ]),
-      transition('* => void', [
-        group([
-          animate(100, style({
-            color: 'red'
-          })),
-          animate(800, style({
-            transform: 'translateX(100px)',
-            opacity: 0
-          })),
-        ])
-      ]),
-    ])
-  ]
-
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  dstate = 'normal';
-  wildstate = 'normal';
-  list = ['Milk', 'Sugar', 'Bread'];
+export class AppComponent implements OnInit {
+  posts: Post[] = [];
 
-    onAnimate() {
-      this.dstate === 'normal' ? this.dstate = 'highlighted' : this.dstate = 'normal';
-      this.wildstate === 'normal' ? this.wildstate = 'highlighted' : this.wildstate = 'normal';
-    }
+  constructor(private http: HttpClient) {}
 
-    onShrink() {
-      this.wildstate = 'shrunken';
-    }
-
-    onAdd(item) {
-      this.list.push(item);
-    }
-
-    onDelete(item) {
-      this.list.splice(this.list.indexOf(item), 1);
-    }
-
-    animationStarted(event) {
-      console.log(event);
-    }
-
-    animationEnded(event) {
-      console.log(event);
-    }
+  ngOnInit() {
+    this.http
+      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, PostComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
