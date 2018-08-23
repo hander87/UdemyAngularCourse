b978c53a5db657f4235516f75166b079c161bed0
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-08-23 15:14:30 +0200

    Section INIT Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { AuthModule } from './auth/auth.module';
-import { CoreModule } from './core/core.module';
-import { reducers } from './store/app.reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-
-import { AuthEffects } from './auth/store/auth.effects';
-
-// GETTING APP INFO
-import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
-    HttpClientModule,
-    AppRoutingModule,
-    SharedModule,
-    ShoppingListModule,
-    AuthModule,
-    CoreModule,
-    StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects]),
-    StoreRouterConnectingModule,
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-  // GETTING APP INFO
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object,
-    @Inject(APP_ID) private appId: string) {
-    const platform = isPlatformBrowser(platformId) ?
-      'in the browser' : 'on the server';
-    console.log(`Running ${platform} with appId=${appId}`);
-  }
- }
+export class AppModule { }
