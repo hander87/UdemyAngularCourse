1a72518bb38b74da5371821965df049428429146
Hanneses <handerblad@Hanness-MacBook-Pro.local>
2018-09-01 15:07:08 +0200

    Adding effect

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -16,25 +16,26 @@ export class DataStorageService {
   storeRecipes() {
     // const headers = new HttpHeaders().set('Authorization', 'Bearer afdklasflaldf');
 
-    // return this.httpClient.put('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    // return this.httpClient.put('https://udemyangularcourse-courseapp.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
     //   observe: 'body',
     //   params: new HttpParams().set('auth', token)
     //   // headers: headers
     // });
-    const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {reportProgress: true});
+    const req = new HttpRequest('PUT', 'https://udemyangularcourse-courseapp.firebaseio.com/recipes.json',
+                                this.recipeService.getRecipes(), {reportProgress: true});
     return this.httpClient.request(req);
   }
 
   getRecipes() {
-    // this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json?auth=' + token)
-    this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
+    // this.httpClient.get<Recipe[]>('https://udemyangularcourse-courseapp.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://udemyangularcourse-courseapp.firebaseio.com/recipes.json', {
       observe: 'body',
       responseType: 'json'
     })
       .map(
         (recipes) => {
           console.log(recipes);
-          for (let recipe of recipes) {
+          for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
             }
